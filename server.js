3402cff4b9adffaa6502b7def8f1a3a33f18774b
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-18 19:03:17 -0700

    changed index.html route to '*' but has bug

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 //routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html'))); //* does not open /notes
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
