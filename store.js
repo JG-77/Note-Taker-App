0c27e7fe5d40ade6c1eb7306cc3c46348162253a
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-20 17:52:07 -0700

    format cchange to store.js

--- store.js
+++ store.js
@@ -1,11 +1,13 @@
 const dbFile = require('./db/db.json');
 
 class Store{ 
-    read() { return readFileAsync(dbFile) // or 'db'?
-
+    read() { 
+        return readFileAsync(dbFile) // or 'db'?
     }
     
     write(note) {
        return writeFileAsync(dbFile, JSON.stringify(note)); //'db/db.json'
      }
 }
+
+module.exports = Store;
