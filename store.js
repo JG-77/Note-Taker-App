69af85cb92313223ef0e3aecb2933a59fc74bb5e
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-20 17:51:27 -0700

    created store.js file to add read and write notes to display on screen

---
+++ store.js
@@ -0,0 +1,11 @@
+const dbFile = require('./db/db.json');
+
+class Store{ 
+    read() { return readFileAsync(dbFile) // or 'db'?
+
+    }
+    
+    write(note) {
+       return writeFileAsync(dbFile, JSON.stringify(note)); //'db/db.json'
+     }
+}
